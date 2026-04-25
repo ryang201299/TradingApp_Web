83f83ddcf8f539dd74c553ace5e81511156bf16f
Ryan Gorman <ryang201299@gmail.com>
2026-04-26 00:17:38 +0100

    Added unrealised returns to accounts page

--- my-app/src/Components/AccountCard.jsx
+++ my-app/src/Components/AccountCard.jsx
@@ -1,6 +1,7 @@
 import styles from './AccountCard.module.css'
 
-function AccountCard({accountName, holdings}) {
+function AccountCard({accountName, holdings, returns}) {
+    const color = returns > 0 ? '#22c55e' : returns < 0 ? '#ef4444' : 'white';
 
     return (
         <>
@@ -8,6 +9,7 @@ function AccountCard({accountName, holdings}) {
                 <h4 className={styles.text}>{accountName}</h4>
                 <div id="account-details">
                     <h5 className={styles.text}>{holdings}</h5>
+                    <h5 className={styles.text} style={{color}}>{returns}</h5>
                 </div>
             </div>
         </>
--- my-app/src/Pages/Accounts.jsx
+++ my-app/src/Pages/Accounts.jsx
@@ -3,32 +3,40 @@ import AccountCard from '../Components/AccountCard.jsx'
 import styles from './Accounts.module.css'
 
 function Accounts() {
-  const [accounts, setAccounts] = useState([])
-  const [accountHoldings, setAccountHoldings] = useState([])
+  const [data, setData] = useState({ accounts: [], holdings: [], performance: [] })
 
   useEffect(() => {
     const fetchData = async () => {
-      const [accountsRes, accountHoldingsRes] = await Promise.all([
+      const [accountsRes, accountHoldingsRes, accountPerformanceRes] = await Promise.all([
         fetch('https://localhost:7295/api/accounts'),
-        fetch('https://localhost:7295/api/holdings')
+        fetch('https://localhost:7295/api/holdings'),
+        fetch('https://localhost:7295/api/performance/unrealisedreturns')
       ])
-      setAccounts(await accountsRes.json())
-      setAccountHoldings(await accountHoldingsRes.json())
+
+      setData({
+        accounts: await accountsRes.json(),
+        holdings: await accountHoldingsRes.json(),
+        performance: await accountPerformanceRes.json()
+      })
     }
     fetchData()
   }, [])
 
   return (
     <div className={styles.background}>
-      {accounts.map(account => {
-        const holding = accountHoldings.find(h => h.account.accountId === account.accountId).holding;
+      {data.accounts.map(account => {
+        const holding = data.holdings.find(h => h.account.accountId === account.accountId)?.holding;
+        const performance = data.performance.find(p => p.account.accountId === account.accountId)?.unrealisedReturns;
+
+        console.log(account, holding, performance)
 
         return (
-          <AccountCard
+        <AccountCard
             key={account.accountId}
             accountName={account.name}
             holdings={holding}
-          />
+            returns={performance}
+        />
         );
       })}
     </div>
